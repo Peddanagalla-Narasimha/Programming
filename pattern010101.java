74039e24ebd1ebf263726b1d978b38498f8a548d
Peddanagalla-Narasimha <narasimhapeddanagalla@gmail.com>
2024-07-17 22:31:26 +0530

    Add files via upload
    Java program to print the 010101 pattern by using single print statement

---
+++ pattern010101.java
@@ -0,0 +1,15 @@
+public class pattern010101 {
+    public static void main(String[] args) {
+		
+        int n = 5;
+
+        for(int i = 1; i <= n; i++)
+        {
+        	
+           System.out.println(i % 2 == 0 ? 0 : 1);
+            
+        }
+       
+    }
+    
+}
